cf84fca6b087e32ee5291d85004d39c453ea01f7
Quang Pham <quangpld@exoplatform.com>
2012-06-15 13:54:16 +0700

    FORUM-4 | Injection FAQ Data

--- extras/injection/src/main/java/org/exoplatform/forum/extras/injection/faq/CategoryInjector.java
+++ extras/injection/src/main/java/org/exoplatform/forum/extras/injection/faq/CategoryInjector.java
@@ -8,42 +8,38 @@ public class CategoryInjector extends AbstractFAQInjector {
 
   /** . */
   private static final String NUMBER = "number";
+  
+  /** . */
+  private static final String USER_PREFIX = "userPrefix";
 
   /** . */
   private static final String CATEGORY_PREFIX = "catPrefix";
 
   @Override
   public void inject(HashMap<String, String> params) throws Exception {
-
-    //
     int number = param(params, NUMBER);
+    String userPrefix = params.get(USER_PREFIX);
     String categoryPrefix = params.get(CATEGORY_PREFIX);
-    init(null, categoryPrefix, null, null, null, 0);
-
+    init(userPrefix, categoryPrefix, null, null, null, 0);
      
     Category rootCategory = getCategoryRoot(true);
-    //
-    for(int i = 0; i <= number; ++i) {
-
-        //
-        String categoryName = categoryName();
-
-        Category cat = new Category();
-        cat.setName(categoryName);
-        cat.setDescription(lorem.getParagraphs(1));
-        cat.setIndex(i);
-        
-        faqService.saveCategory(rootCategory.getId(), cat, true);
-        
-        ++categoryNumber;
-
-        //
-        getLog().info("Category '" + categoryName + "' created with parent is '" + rootCategory + "'");
-
-    }
+    String categoryName = null;
+    Category cat = null;
     
-  }
-  
-  
+    for(int i = 0; i < number; i++) {
+      categoryName = categoryName();
 
+      cat = new Category();
+      cat.setIndex(i);
+      cat.setName(categoryName);
+      cat.setDescription(lorem.getParagraphs(1));
+      cat.setModerators(new String[] {USERS.get(0)});
+      cat.setUserPrivate(new String[] {""});
+      
+      faqService.saveCategory(rootCategory.getPath(), cat, true);
+      categoryNumber++;
+      
+      getLog().info("Category '" + categoryName + "' created with parent is '" + rootCategory.getId() + "'");
+    }
+  }
 }
