88afa91f98e97aa5b498bf04e6f1cac5f7d2a20f
Quang Pham <pldquang@gmail.com>
2012-07-17 02:47:57 +0700

    20120717 - FORUM-15 | Migrate JS to use JQuery for Forum+Poll portlet

--- forum/webapp/src/main/webapp/javascript/eXo/forum/CheckBox.js
+++ forum/webapp/src/main/webapp/javascript/eXo/forum/CheckBox.js
@@ -1,44 +1,48 @@
-eXo.forum.CheckBox = {
-  init : function(cont){
-    if(typeof(cont) == "string") cont = document.getElementById(cont) ;
-    if(cont){
-      var checkboxes = eXo.core.DOMUtil.findDescendantsByClass(cont, "input", "checkbox") ;
-      if(checkboxes.length <=0) return ;
-      checkboxes[0].onclick = this.checkAll ;
-      var len = checkboxes.length ;
-      for(var i = 1 ; i < len ; i ++) {
-        checkboxes[i].onclick = this.check ;
-        eXo.forum.CheckBoxManager.checkItem(checkboxes[i]);
+;(function($, window, document) {
+  var CheckBox = {
+    init : function(cont) {
+      if (typeof(cont) == 'string') cont = findId(cont) ;
+      if (cont.exists()){
+        var checkboxes = cont.find('input.checkbox');
+        if (!checkboxes.exists()) return ;
+        checkboxes.eq(0).on('click', this.checkAll);
+        var len = checkboxes.length ;
+        for(var i = 1 ; i < len ; i ++) {
+          checkboxes.eq(i).on('click', this.check);
+          eXo.forum.CheckBoxManager.checkItem(checkboxes.eq(i));
+        }
+      }
+    },
+    check : function(){
+      eXo.forum.CheckBoxManager.checkItem(this);
+      var row = $(this).parents('tr');
+      if (this.checked) {
+        row.addClass('SelectedItem');
+        eXo.forum.UIForumPortlet.setChecked(true);
+      }else{
+        eXo.forum.UIForumPortlet.setChecked(false);
+        row.removeClass('SelectedItem');
+      }
+    },
+    checkAll : function(){
+      eXo.forum.UIForumPortlet.checkAll(this);
+      var table = $(this).find('table tbody').eq(0);
+      var rows = $(table).find('tr');
+      var i = rows.length ;
+      if (this.checked) {
+        while(i--) {
+          rows.eq(i).addClass('SelectedItem');
+        }
+      } else{
+        while(i--){
+          rows.eq(i).removeClass('SelectedItem');
+        }
       }
     }
-  },
-  
-  check : function(){
-    eXo.forum.CheckBoxManager.checkItem(this);
-    var row = eXo.core.DOMUtil.findAncestorByTagName(this,"tr");
-    if(this.checked) {
-      eXo.core.DOMUtil.addClass(row,"SelectedItem");
-      eXo.forum.UIForumPortlet.setChecked(true);
-    }else{
-      eXo.forum.UIForumPortlet.setChecked(false);
-      eXo.core.DOMUtil.replaceClass(row,"SelectedItem","");
-    }
-  },
-  
-  checkAll : function(){
-    eXo.forum.UIForumPortlet.checkAll(this);
-    var table = eXo.core.DOMUtil.findAncestorByTagName(this,"table");
-    table = eXo.core.DOMUtil.getChildrenByTagName(table,"tbody")[0];
-    var rows = eXo.core.DOMUtil.findDescendantsByTagName(table,"tr");
-    var i = rows.length ;
-    if(this.checked){
-      while(i--) {
-        eXo.core.DOMUtil.addClass(rows[i],"SelectedItem");
-      }
-    } else{
-      while(i--){
-        eXo.core.DOMUtil.replaceClass(rows[i],"SelectedItem","");
-      }
-    }
-  }
-} ;
+  };
+
+  // Expose
+  window.eXo = eXo || {};
+  window.eXo.forum = eXo.forum || {} ;
+  window.eXo.forum.CheckBox = CheckBox;
+})(gj, window, document);
