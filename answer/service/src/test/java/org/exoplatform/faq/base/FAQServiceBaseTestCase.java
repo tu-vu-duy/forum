857e806ad26aa8671bf0a1424e721f9b27eb0f39
rtouzi <rtouzi@exoplatform.com>
2014-02-14 12:35:45 +0100

    PLF-5233 : Full Integration of INSP in platform.

--- answer/service/src/test/java/org/exoplatform/faq/base/FAQServiceBaseTestCase.java
+++ answer/service/src/test/java/org/exoplatform/faq/base/FAQServiceBaseTestCase.java
@@ -44,7 +44,7 @@ import org.exoplatform.services.security.MembershipEntry;
 @ConfiguredBy({
   @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.portal-configuration.xml"),
   @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.identity-configuration.xml"),
-  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.faq.component.test.jcr-configuration.xml"),
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.test.jcr-configuration.xml"),
   @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/test-portal-configuration.xml"),
   @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.faq.component.core.test.configuration.xml"),
   @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.faq.test.jcr-configuration.xml"),
